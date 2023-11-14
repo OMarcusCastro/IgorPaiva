2e57a597d14a438557e54b0a9428b72e0fea97a1
Gordom12 <123670251+Gordom12@users.noreply.github.com>
2023-11-14 22:22:12 +0000

    Co-authored-by: Marcus Castro <OMarcusCastro@users.noreply.github.com>

--- Back end Node/project 01 nodejs/streams/fundamentos.js
+++ Back end Node/project 01 nodejs/streams/fundamentos.js
@@ -1,30 +1,38 @@
-import { Readable } from "node:stream"
-import { Writable } from "node:stream"
+import { Readable } from "stream";
+import { Writable } from "stream";
+import { Transform } from "stream";
 
-class OneToHundreadSteam extends Readable{
-    index = 1 
-    _read(){
-        const i = this.index++
-        setTimeout(()=>{
-            if(i>100){
-                this.push(null)
-            } else{
-                const buf = Buffer.from(String(i))
-                this.push(buf)
-            }
-            
-        },1000);
+class OneToHundredStream extends Readable {
+    index = 1
+    _read() {
+      const i = this.index++
+      setTimeout(() => {
+        if (i > 100) {
+          this.push(null)
+        } else {
+          const buf = Buffer.from(String(i))
+          this.push(buf)
+        }
+      }, 1000);
+    }
+  }
+
+class InverseNumberStream extends Transform {
+    _transform(chunk, encoding, callback) {
+        const transformed = Number(chunk.toString()) * -1
+        callback (null, Buffer.from(String(transformed)))
     }
 }
 
-class MultiplyByTenStream extends Writable(){
-    
-    _write(chunck,encoding,callback){
-        //processar dados 
-        console.log(Number(chunck.toString())*10)
-        callback()
+class MultiplyByTenStream extends Writable {
+    _write(chunk, encoding, callback) {
+      console.log(Number(chunk.toString()) * 10)
+      callback()
     }
-}
-
-new OneToHundreadSteam()
+  }
+  
+new OneToHundredStream()
+    .pipe( new InverseNumberStream())
     .pipe(new MultiplyByTenStream())
+
+
---
+++ Back end Node/project 02 nodejs/src/server.js
@@ -0,0 +1,6 @@
+function user(usuario) {
+    console.log(usuario.idade);
+    console.log("ola");
+}
+user({ idade: 20 });
+
